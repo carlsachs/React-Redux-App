4975611374f9c48b1e684c7c48213ef673dc8f8d
carlsachs <sachscarl@gmail.com>
2020-02-19 17:26:54 -0500

    finished axios call in actions file

--- my-app/src/actions/actions.js
+++ my-app/src/actions/actions.js
@@ -6,5 +6,13 @@ export const IS_ERROR = "SET_ERROR";
 
 export const getData = () => dispatch => {
     dispatch({ type: FETCH_DATA });
-    axios.get("")
+    axios.get("https://coingecko.com/api/documentations/v3")
+    .then(res => {
+        console.log(res);
+        dispatch({ type: UPDATE_ITEMS, payload: res.data });
+    })
+    .catch(err => {
+        console.log("error fetching API from coingecko", err);
+        dispatch({ type: IS_ERROR, payload: "Error fetching data from coinGecko." });
+    })
 }
