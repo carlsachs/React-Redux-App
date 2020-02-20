885033a5c78404d1b6c71c6cab1b8efba5a96f56
carlsachs <sachscarl@gmail.com>
2020-02-19 21:40:51 -0500

    styling done for now

--- my-app/src/components/Container.js
+++ my-app/src/components/Container.js
@@ -8,13 +8,30 @@ const Boxes = styled.div`
     flex-flow: row wrap;
 `;
 
+const Button = styled.button`
+    width: 25%;
+    height: 50px;
+    background: rgb(0,2,6);
+    background: radial-gradient(circle, rgba(0,2,6,0.938813025210084) 0%, rgba(0,10,41,0.908000700280112) 100%);
+    color: #3973ac;
+    font-size: 1.15rem;
+    border: none;
+    :hover {
+        border:none;
+        transition: all 1s ease-in-out;
+        box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
+        background: rgb(195,215,255);
+        background: radial-gradient(circle, rgba(195,215,255,0.938813025210084) 0%, rgba(86,126,255,0.908000700280112) 100%);
+    }
+`;
+
 
 const Container = props => {
 
     console.log("container", props)
     return (
         <div>
-            <button onClick={props.handleGetData}>Get Current Pricing</button>
+            <Button onClick={props.handleGetData}>Get Current Pricing</Button>
             {props.prices.length ? (
                 <Boxes>
                    {props.prices.map(price => (
@@ -22,7 +39,7 @@ const Container = props => {
                    ))}
                 </Boxes>
              ) : (
-                <p>Ready for a Heartbreak??</p>
+                <p>^^^ Only Click If Prepared For Heartbreak ^^^</p>
              )
           }
         </div>
